2d502c080bbb0aa9b501bce1f456794874c57111
GreenXenith <bl00ntdmonkey@yahoo.com>
2017-03-08 13:40:01 -0800

    Add aliases for compatibility with Minetest versions >0.4.15

--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
-dofile(minetest.get_modpath("sandplus").."/nodes.lua")
-dofile(minetest.get_modpath("sandplus").."/crafts.lua")
+if minetest.registered_nodes["default:desert_sandstone"] then
+	-- Aliases for sandplus node names
+	minetest.register_alias("sandplus:desert_sandstone", "default:desert_sandstone")
+	minetest.register_alias("sandplus:desert_sandstone_block", "default:desert_sandstone_block")
+	minetest.register_alias("sandplus:desert_sandstone_brick", "default:desert_sandstone_brick")
+	minetest.register_alias("sandplus:silver_sandstone", "default:silver_sandstone")
+	minetest.register_alias("sandplus:silver_sandstone_block", "default:silver_sandstone_block")
+	minetest.register_alias("sandplus:silver_sandstone_brick", "default:silver_sandstone_brick")
+else
+	dofile(minetest.get_modpath("sandplus").."/nodes.lua")
+	dofile(minetest.get_modpath("sandplus").."/crafts.lua")
+end
